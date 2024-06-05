dca42502e52c93b8ad2adb2b4419224950a0f058
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-05 13:01:06 +0700

    feat:add greeting message func

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+
+  const greetingMessageHandle = (message) => {
+    setGreetingMessage(message);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => greetingMessageHandle("สวัสดี!")}>
+          สวัสดี!
+        </button>
+        <button onClick={() => greetingMessageHandle("Hi!")}>Hi!</button>
+        <button onClick={() => greetingMessageHandle("你好!")}>你好!</button>
       </div>
     </div>
   );
